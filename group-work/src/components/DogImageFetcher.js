383b5a42d52fcd4c7b7192b5f594b2e8445aaaec
OpenWindow231312 <231312@virtualwindow.co.za>
2025-03-20 12:10:48 +0200

    new

--- group-work/src/components/DogImageFetcher.js
+++ group-work/src/components/DogImageFetcher.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./DogImageFetcher.css"; // Import the updated CSS
+import "./DogImageFetcher.css";
 
 const DogImageFetcher = () => {
   const [imageUrl, setImageUrl] = useState("");
+
+  // Function to play the bark sound
+  const playBarkSound = () => {
+    const audio = new Audio("sounds/dogbark.mp3); 
+    audio.volume = 0.5; // Adjust volume (0.0 to 1.0)
+    audio.play();
+  };
 
   const fetchDogImage = async () => {
     try {
@@ -11,6 +18,7 @@ const DogImageFetcher = () => {
         "https://dog.ceo/api/breeds/image/random"
       );
       setImageUrl(response.data.message);
+      playBarkSound(); 
     } catch (error) {
       console.error("Error fetching the dog image:", error);
     }
